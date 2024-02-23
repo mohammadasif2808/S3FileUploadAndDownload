4c77b3c1c9b1f30ca5dd98dc02ce82f45afaef0a
mohammadasif2808 <mohammadasif2808@gmail.com>
2024-02-23 09:19:53 +0530

    Initial commit

---
+++ src/main/java/in/co/mohammadasif/S3FileUploadAndDownload.java
@@ -0,0 +1,11 @@
+package in.co.mohammadasif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3FileUploadAndDownload {
+    public static void main(String[] args) {
+        SpringApplication.run(S3FileUploadAndDownload.class,args);
+    }
+}
